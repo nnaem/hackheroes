06220803a342b05052cfd96de7db8ad496813b68
Canny <canny1913@outlook.com>
2022-10-14 22:27:54 +0300

    fix theme switcher

--- app/src/main/java/com/hackheroes/newoldtown/MainActivity.kt
+++ app/src/main/java/com/hackheroes/newoldtown/MainActivity.kt
@@ -8,18 +8,28 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.with
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
+import com.hackheroes.newoldtown.preferences.PreferencesManager
 import com.hackheroes.newoldtown.ui.navigation.AppDestination
 import com.hackheroes.newoldtown.ui.screen.MainHomeScreen
+import com.hackheroes.newoldtown.ui.theme.Theme
 import com.xinto.taxi.Taxi
 import com.xinto.taxi.rememberBackstackNavigator
+import org.koin.android.ext.android.inject
 
 class MainActivity : ComponentActivity() {
+    private val prefs: PreferencesManager by inject()
+
     @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Theme(
+                dynamicColor = prefs.dynamicColor,
+                darkTheme = prefs.theme == Theme.SYSTEM && isSystemInDarkTheme() || prefs.theme == Theme.DARK,
+            ) {
                 val navigator = rememberBackstackNavigator<AppDestination>(AppDestination.Home)
 
                 BackHandler {
@@ -41,3 +51,4 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
